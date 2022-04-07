b1192221d8f00eeba251eba3c9fe7b5e04249662
Abhishek Manoj Jangid <amanojja@tibco.com>
2022-04-07 12:52:51 +0530

    URL of CLI APIs updated

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,8 +1,8 @@
 export const CLIAPIS = {
-    getapis:'/modeler/v1/cli/getAPIs',
+    getapis:'/modeler/v1/cli/listAPIs',
     getprojects:'/modeler/v1/cli/getProjects',
     exportapis:'/modeler/v1/cli/export',
-    importapis:'/modeler/v1/cli/import',
-    validateapis:'modeler/v1/cli/validate',
+    importapis:'/modeler/v1/cli/importAPIs',
+    validateapis:'modeler/v1/cli/validateAPIs',
     createproject:'/modeler/v1/cli/createProject'
 }
